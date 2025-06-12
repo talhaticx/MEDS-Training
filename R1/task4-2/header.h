23f63cb4ca606ed7190eb045f850e11ab68b5e95
talhaticx <mtalhaayaz2005@gmail.com>
2025-06-12 18:30:46 +0500

    task 4 completed

---
+++ R1/task4-2/header.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+
+int add(int a, int b);  // Function prototype
+
+#endif  // HEADER_H
---
+++ R1/task4-2/main.c
@@ -0,0 +1,8 @@
+#include "header.h"
+
+int main() {
+    int a = 5, b = 7;
+    int sum = add(a, b);
+    printf("Sum: %d\n", sum);
+    return 0;
+}
